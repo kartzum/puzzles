e78f1fb3d83e40ba958d802909d356463c92f12b
akoval <c@apple456>
2018-06-22 18:18:25 +0300

    easy. cos2.

--- easy/src/test/scala/com/n/a/puzzles/easy/EasyMathSuite.scala
+++ easy/src/test/scala/com/n/a/puzzles/easy/EasyMathSuite.scala
@@ -12,4 +12,13 @@ class EasyMathSuite extends FunSuite with BeforeAndAfter {
     assert(EasyMath.tolerance(EasyMath.cos(math.toRadians(10)), math.cos(math.toRadians(10))))
     assert(EasyMath.tolerance(EasyMath.cos(math.toRadians(45)), math.cos(math.toRadians(45))))
   }
+
+  test("cos2(0, 90, 180, 10, 45)") {
+    assert(EasyMath.tolerance(EasyMath.cos2(math.toRadians(0)), math.cos(math.toRadians(0))))
+    assert(EasyMath.tolerance(EasyMath.cos2(math.toRadians(90)), math.cos(math.toRadians(90)), 0.1))
+    assert(EasyMath.tolerance(EasyMath.cos2(math.toRadians(180)), math.cos(math.toRadians(180))))
+
+    assert(EasyMath.tolerance(EasyMath.cos2(math.toRadians(10)), math.cos(math.toRadians(10))))
+    assert(EasyMath.tolerance(EasyMath.cos2(math.toRadians(45)), math.cos(math.toRadians(45))))
+  }
 }
