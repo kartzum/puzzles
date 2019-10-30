06f4183eafffcfe4e8dd97a243257568a1a83b0a
akoval <c@apple456>
2019-10-30 10:55:11 +0300

    tasks. example.

---
+++ tasks/src/main/scala/com/n/a/puzzles/tasks/Example.scala
@@ -0,0 +1,7 @@
+package com.n.a.puzzles.tasks
+
+object Example {
+  def sum(a: Int, b: Int): Int = {
+    a + b
+  }
+}
---
+++ tasks/src/test/scala/com/n/a/puzzles/tasks/ExampleSuite.scala
@@ -0,0 +1,9 @@
+package com.n.a.puzzles.tasks
+
+import org.scalatest.FunSuite
+
+class ExampleSuite extends FunSuite {
+  test("example") {
+    assert(Example.sum(2, 2) == 4)
+  }
+}
